cc39a5442fe5a0dc6b918156c041272a216a0b38
Chen Yu <scheduledx@gmail.com>
2023-02-04 15:27:53 +0800

    combine files

--- src/script.js
+++ src/script.js
@@ -1,3 +1,13 @@
+// ==UserScript==
+// @name         Discord Chat Replay
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Rewind past Discord Chat activity in realtime.
+// @author       Cerlancism
+// @match        https://discord.com/channels/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=discord.com
+// ==/UserScript==
+
 //@ts-check
 
 (function ()

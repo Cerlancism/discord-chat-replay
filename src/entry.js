4a399150dc2f2d2f71c5e1f7fb5544af325b8a60
Chen Yu <scheduledx@gmail.com>
2023-02-04 15:28:14 +0800

    Update entry.js

--- src/entry.js
+++ src/entry.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  Rewind past Discord Chat activity in realtime.
 // @author       Cerlancism
-// @match        https://discord.com/channels/*
+// @match        https://discord.com/channels/*/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=discord.com
 // @require      file:///C:/Users/CHE/Documents/Github/discord-chat-replay/src/script.js
 // ==/UserScript==
